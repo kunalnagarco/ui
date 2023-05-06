7a70ed17ab05c8b05ab2598ec809c30558e8f0fb
Kunal Nagar <2741371+kunalnagar@users.noreply.github.com>
2023-05-06 16:16:15 -0400

    Fix fetch depth

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -2,7 +2,6 @@ module.exports = {
   extends: ['react-app', 'react-app/jest', 'prettier'],
   plugins: ['import'],
   rules: {
-    // Plugin to decide import order
     'import/order': [
       'error',
       {
