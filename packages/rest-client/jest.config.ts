19b15514623e8c80b7fed46986ca3c771818d607
Kunal Nagar <2741371+kunalnagar@users.noreply.github.com>
2023-05-06 13:46:22 -0400

    Add rest-client and jest-config

---
+++ packages/rest-client/jest.config.ts
@@ -0,0 +1,7 @@
+import { ReactConfig, Config } from '@kunalnagarco/ui.jest-config';
+
+const config: Config = {
+  ...ReactConfig,
+};
+
+export default config;
--- packages/rest-client/src/client.ts
+++ packages/rest-client/src/client.ts
@@ -38,6 +38,73 @@ export interface RestClientContract {
   setBaseUrl: (baseUrl: string) => void;
 }
 
-// export class RestClient implements RestClientContract {
-//   private _baseUrl: string;
-// }
+export class RestClient implements RestClientContract {
+  private _baseUrl: string;
+
+  constructor({ baseUrl }: RestClientOptions) {
+    this._baseUrl = baseUrl;
+  }
+
+  private _getFullUrl(url: string): string {
+    return `${this._baseUrl}${url}`;
+  }
+
+  private _requestFacade<T>(
+    method: string,
+    { url, body, headers }: RestClientMethodOptions,
+  ): Promise<RestClientResponse<T>> {
+    const fetchPromise = fetch(this._getFullUrl(url), {
+      method,
+      headers,
+      body,
+    });
+    return new Promise<RestClientResponse<T>>(async (resolve, reject) => {
+      const response = await fetchPromise;
+      let data;
+      try {
+        data = await response.json();
+      } catch (err) {
+        data = null;
+      }
+      const returnObj = {
+        headers: response.headers,
+        data,
+        ok: response.ok,
+        status: response.status,
+        statusText: response.statusText,
+      };
+      if (response.ok) {
+        return resolve(returnObj);
+      }
+      return reject(returnObj);
+    });
+  }
+
+  setBaseUrl(baseUrl: string): void {
+    this._baseUrl = baseUrl;
+  }
+
+  async get<T>(
+    options: RestClientMethodOptions,
+  ): Promise<RestClientResponse<T>> {
+    return this._requestFacade('GET', options);
+  }
+
+  async post<T>(
+    options: RestClientMethodOptions,
+  ): Promise<RestClientResponse<T>> {
+    return this._requestFacade('POST', options);
+  }
+
+  async put<T>(
+    options: RestClientMethodOptions,
+  ): Promise<RestClientResponse<T>> {
+    return this._requestFacade('PUT', options);
+  }
+
+  async delete<T>(
+    options: RestClientMethodOptions,
+  ): Promise<RestClientResponse<T>> {
+    return this._requestFacade('DELETE', options);
+  }
+}
