741d5c50650cf6c362238bd8aa9a16a6f5922d23
Kunal Nagar <2741371+kunalnagar@users.noreply.github.com>
2024-04-16 20:48:48 -0400

    Update plugins

--- packages/eslint-config/src/index.js
+++ packages/eslint-config/src/index.js
@@ -1,12 +1,19 @@
 import airbnbConfig from 'airbnb';
 import airbnbTypescriptConfig from 'airbnb-typescript';
 import prettierConfig from 'prettier';
+import importPlugin from 'eslint-plugin-import';
+import typescriptEslintPlugin from '@typescript-eslint';
+import jestPlugin from 'jest';
 
 module.exports = {
   airbnbConfig,
   airbnbTypescriptConfig,
   prettierConfig,
-  plugins: ['import', '@typescript-eslint', 'jest'],
+  plugins: {
+    importPlugin,
+    typescriptEslintPlugin,
+    jestPlugin,
+  },
   env: {
     browser: true,
     es6: true,
