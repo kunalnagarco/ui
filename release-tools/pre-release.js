d722a528f6c4a9f5a47efeb28735a623e0033d2c
Kunal Nagar <2741371+kunalnagar@users.noreply.github.com>
2025-06-03 00:04:58 -0400

    Initial commit

--- release-tools/pre-release.js
+++ release-tools/pre-release.js
@@ -3,37 +3,22 @@ const { readFileSync } = require("fs");
 
 async function main() {
   const pathPrefix = ".";
-  const exclude = [];
-
   const packages = await glob(`${pathPrefix}/packages/*`, {
     deep: 1,
     onlyDirectories: true,
   });
-  console.log(packages);
   for (const dir of packages) {
     const before = process.cwd();
     cd(dir);
     const packageJsonStr = readFileSync("package.json", {
       encoding: "utf8",
     });
-    const { name, version } = JSON.parse(packageJsonStr);
-    console.log(name, version);
-    // const hash = new Date().getTime();
-    // await $`yarn version --deferred ${version}-alpha-${hash}`;
-    await $`yarn version --deferred patch`;
-    // const packageJsonStr2 = readFileSync("package.json", {
-    //   encoding: "utf8",
-    // });
-    // const { name2, version2 } = JSON.parse(packageJsonStr2);
-    // console.log(name2, version2);
+    const { version } = JSON.parse(packageJsonStr);
+    const hash = new Date().getTime();
+    await $`yarn version --immediate ${version}-alpha-${hash}`;
+    await $`yarn npm publish --tag alpha`;
     cd(before);
-    // if (exclude.some((p) => dir.endsWith(p))) {
-    //   console.log(`skipping build for ${dir}`);
-    //   continue;
-    // }
-    // console.log(dir);
   }
-  await $`yarn version apply --all --prerelease --dry-run`;
 }
 
 main();
