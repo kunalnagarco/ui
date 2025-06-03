8e7a58996b347dbffca8d49957b28b7e33aa216c
Kunal Nagar <2741371+kunalnagar@users.noreply.github.com>
2025-06-03 00:30:37 -0400

    Update

--- release-tools/pre-release.js
+++ release-tools/pre-release.js
@@ -1,3 +1,7 @@
+/**
+ * For every commit made, GitHub actions will release an alpha version to npm
+ * for each package in the yarn workspace.
+ */
 const { $, cd, glob } = require("zx");
 const { readFileSync } = require("fs");
 
@@ -17,9 +21,18 @@ async function main() {
     const { version } = JSON.parse(packageJsonStr);
     const hash = new Date().getTime();
     await $`yarn version --immediate ${version}-alpha-${hash}`;
-    await $`yarn npm publish --tag alpha`;
+    await $`yarn npm publish --tag alpha --provenance`;
     cd(before);
   }
 }
 
-main();
+/**
+ * This script should only be run on GitHub actions. There is an explicit
+ * check for the GitHub actions environment to prevent this script from publishing
+ * versions from local environments.
+ *
+ * More info: https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+ */
+if (process.env.CI) {
+  main();
+}
