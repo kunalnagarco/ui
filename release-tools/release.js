ee84c40e975a71cd0dc1c70d9c7733cc08818acc
Kunal Nagar <2741371+kunalnagar@users.noreply.github.com>
2025-06-03 10:27:34 -0400

    Update test new

---
+++ release-tools/release.js
@@ -0,0 +1,43 @@
+/**
+ * When merging to main, GitHub actions will release a version to npm
+ * for each package in the yarn workspace.
+ */
+const { $, cd, glob } = require("zx");
+const { readFileSync } = require("fs");
+
+/**
+ * 1. create git tag + github release
+ * 2. create npm version + tag (latest)
+ * 3. update the new version back into the repo (push from github actions)
+ */
+async function main() {
+  // const pathPrefix = ".";
+  // const packages = await glob(`${pathPrefix}/packages/*`, {
+  //   deep: 1,
+  //   onlyDirectories: true,
+  // });
+  // $.verbose = true;
+  // for (const dir of packages) {
+  //   const before = process.cwd();
+  //   cd(dir);
+  //   const packageJsonStr = readFileSync("package.json", {
+  //     encoding: "utf8",
+  //   });
+  //   const { version } = JSON.parse(packageJsonStr);
+  //   const hash = new Date().getTime();
+  //   await $`yarn version --immediate ${version}-alpha-${hash}`;
+  //   await $`yarn npm publish --tag alpha`;
+  //   cd(before);
+  }
+}
+
+/**
+ * This script should only be run on GitHub actions. There is an explicit
+ * check for the GitHub actions environment to prevent this script from publishing
+ * versions from local environments.
+ *
+ * More info: https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/store-information-in-variables#default-environment-variables
+ */
+if (process.env.CI) {
+  main();
+}
